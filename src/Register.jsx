1749dcd2e787d72fbd20c0e959093d1edbda48ae
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-03-20 15:14:33 +0100

    ajout page inscription

---
+++ src/Register.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default function Register() {
+    const [values, setValues] = useState({
+        name: '',
+        surname: '',
+        password: '',
+        email: '',
+        phone: '',
+        anciennete: '',
+    })
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
